3e79b232f5d2fd049fc05c49cc3429fed0949fd4
jim <535258211@qq.com>
2018-08-03 14:18:10 +0800

    维护一个iframe中获取外层dom元素的方法

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,15 @@ $(function () {
         $('#mainFrameTabs').bTabsAdd(menuId,title,url);
         // $('#mainFrameTabs').bTabsAdd(123,'用户管理','http://www.kgc.cn/');
     });
+
+    /**
+     * 获取在iframe最外层dom元素
+     * @param {str} className
+     */
+    window.getWindowDom = function (className){
+        var win = window.parent.$;
+        return win(className);
+    }
     $('#mainFrameTabs').bTabs({
     });
 });
