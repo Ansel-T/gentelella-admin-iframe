ab5a6d8879b3f665ba127b0ec391ad05ac41d260
jim <535258211@qq.com>
2018-08-05 11:07:10 +0800

    提供更新iframe的方法

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,7 @@
 $(function () {
     /* 初始化后台iframe */
+    $('#mainFrameTabs').bTabs({
+    });
     $('a',$('#menuSideBar')).on('click', function(e) {
         e.stopPropagation();
         var li = $(this).closest('li');
@@ -10,14 +12,35 @@ $(function () {
         // $('#mainFrameTabs').bTabsAdd(123,'用户管理','http://www.kgc.cn/');
     });
 
-    /**
-     * 获取在iframe最外层dom元素
-     * @param {str} className
+    window.tools = {
+        getOuterJq:getOuterJq,
+        iframeReload:iframeReload
+    };
+
+    /**javascript:;
+     * 获取iframe 外层jq对象
+     * @param className
+     * @returns {*}
      */
-    window.getWindowDom = function (className){
+    function getOuterJq (className){
+        if(!className) return;
         var win = window.parent.$;
         return win(className);
     }
-    $('#mainFrameTabs').bTabs({
-    });
+
+    /**
+     * 刷新iframe
+     * @param id iframe唯一父元素id
+     * @param level sibling:刷新兄弟iframe ,parent:窗口，默认：当前
+     */
+    function iframeReload(level,id){
+        if(level ==="sibling" && !id) return;
+        var win = window;
+        if(level === 'sibling'){
+            win = getOuterJq('#bTabs_' + id).find('iframe').get(0).contentWindow;
+        }else if(level === 'parent'){
+            win = window.parent;
+        }
+        win.location.reload();
+    }
 });
